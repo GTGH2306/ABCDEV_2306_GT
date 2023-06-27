f6b6a769d0ab2f233e766cb814ba661ac60a60fd
GTGH2306 <137295165+GTGH2306@users.noreply.github.com>
2023-06-27 16:02:58 +0200

    Algo java début

---
+++ Algo_Java/exempleProjet/src/exempleProjet/App.java
@@ -0,0 +1,11 @@
+package exempleProjet;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Coucou tout le monde!");
+
+	}
+
+}
