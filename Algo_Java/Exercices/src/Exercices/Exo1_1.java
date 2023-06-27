88198edaa9f625fc197d5d86f3fa1692cca1128b
GTGH2306 <137295165+GTGH2306@users.noreply.github.com>
2023-06-27 16:57:30 +0200

    sauvegarde soir

---
+++ Algo_Java/Exercices/src/Exercices/Exo1_1.java
@@ -0,0 +1,16 @@
+package Exercices;
+import java.util.Scanner;
+
+public class Exo1_1 {
+	public static void main (String[] args) {
+		float nbr1;
+		float nbr2;
+		float result;
+		
+		scanner = new Scanner(System.in);
+		System.out.println("Entrez la valeur de ");
+		
+		nbr1 = scanner.nextFloa();
+		
+	}
+}
