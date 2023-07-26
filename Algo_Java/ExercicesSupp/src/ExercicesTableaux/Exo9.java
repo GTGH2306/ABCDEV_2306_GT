4f83d1719944f190d1e41446beb70fb1b7f0f6d5
GTGH2306 <137295165+GTGH2306@users.noreply.github.com>
2023-07-26 15:01:28 +0200

    26/07

--- Algo_Java/ExercicesSupp/src/ExercicesTableaux/Exo9.java
+++ Algo_Java/ExercicesSupp/src/ExercicesTableaux/Exo9.java
@@ -1,10 +1,44 @@
 package ExercicesTableaux;
+import java.util.Scanner;
 
 public class Exo9 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		double[] notes;
+		double sommeNotes = 0;
+		double moyenne;
+		int nbrNotes;
+		int intello = 0;
+		
+		
+		System.out.println("Saisissez le nombre d'eleves : ");
+		nbrNotes = sc.nextInt();
+		notes = new double[nbrNotes];
+		
+		for (int i = 0; i < notes.length; i++) {
+			System.out.println("Saisissez la note de l'eleve " + (i+1) + " : ");
+			notes[i] = Math.round(sc.nextDouble() * 100) / 100d;
+			
+			if (notes[i] < 0 || notes[i] > 20) {
+				System.out.println("Saisie invalide.");
+				i--;
+			} else {
+				sommeNotes += notes[i];
+			}
+			
+		}
+		moyenne = Math.round((sommeNotes / nbrNotes) * 100) / 100d;
+		
+		for (int i = 0; i < notes.length; i++) {
+			if (notes[i] > moyenne) {
+				intello ++;
+			}
+		}
+		
+		System.out.println("La moyenne est de " + moyenne + " .\nIl y'a " + intello + " intellos dans votre classe.");
+		sc.close();
+		
 	}
 
 }
