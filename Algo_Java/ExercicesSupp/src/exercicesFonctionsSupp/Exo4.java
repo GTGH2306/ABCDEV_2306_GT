3bc3ea21379bebcef291105768facacafd19b9b7
GTGH2306 <137295165+GTGH2306@users.noreply.github.com>
2023-08-22 16:49:33 +0200

    commit 22/08

--- Algo_Java/ExercicesSupp/src/exercicesFonctionsSupp/Exo4.java
+++ Algo_Java/ExercicesSupp/src/exercicesFonctionsSupp/Exo4.java
@@ -4,22 +4,19 @@ import java.util.Scanner;
 
 public class Exo4 {
 
-	private static String purge(String phrase, char purge) {
-		String result = "";
-		for (int i = 0; i < phrase.length(); i++) {
-			if (phrase.charAt(i) != purge) {
-				result += phrase.charAt(i);
-			}
+	private static String purge(String phrase, String purge) {
+		for (int j = 0; j < purge.length(); j++) {
+			phrase = phrase.replace(Character. toString(purge.charAt(j)), "");
 		}
-		return result;
+		return phrase;
 	}
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Saisissez une phrase :");
 		String phrase = sc.nextLine();
-		System.out.println("Saisissez un caractère à purger :");
-		char purge = sc.nextLine().charAt(0);
+		System.out.println("Saisissez des caractères à purger :");
+		String purge = sc.nextLine();
 		
 		System.out.println(purge(phrase, purge));
 		sc.close();
